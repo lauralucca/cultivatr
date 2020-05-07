1d56dfc3806be92f7207f43b14668466796535ed
lalaura.lourenco <laura.lourenco@thoughtworks.com>
2020-05-07 18:08:05 -0300

    [Laura|Matheus]Creates initial setup

---
+++ api/src/main/java/com/cultivatr/api/CultivatrApplication.java
@@ -0,0 +1,13 @@
+package com.cultivatr.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CultivatrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CultivatrApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/cultivatr/api/CultivatrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cultivatr.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CultivatrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
