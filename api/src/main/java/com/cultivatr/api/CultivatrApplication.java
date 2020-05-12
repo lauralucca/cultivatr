cd11a782de5eedc799ff0263f18245de4da3e928
Matheus Henrique Pereira Santos <lamatheus.santos@MacBook-Pro-de-Matheus.local>
2020-05-12 15:13:17 -0300

    [Laura|Matheus] Defines global CORS configuration

--- api/src/main/java/com/cultivatr/api/CultivatrApplication.java
+++ api/src/main/java/com/cultivatr/api/CultivatrApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
 @RestController
@@ -15,10 +17,24 @@ public class CultivatrApplication {
 		SpringApplication.run(CultivatrApplication.class, args);
 	}
 
-	@CrossOrigin(origins = "http://localhost:3000")
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
+	}
+
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/hello").allowedOrigins("http://localhost:3000");
+
+			}
+		};
+
+
+											
 	}
 
 }
